0fc79de40cc28313111b31c8f938f9bafde6f1ca
Michael Stewart <github@thegreatmichael.com>
2014-01-28 09:39:16 -0500

    Update Wurst.js
    fixes #1

--- www/Wurst.js
+++ www/Wurst.js
@@ -5,7 +5,7 @@ function Wurst() {}
 
 Wurst.prototype.dummy = function() {
         exec(
-            function(result){ alert('ok: '+reply);      },
+            function(reply){ alert('ok: '+reply);      },
             function(err){ alert('Error: '+err); }
         , "Wurst", "echoTest", ['dummy']);
 }
